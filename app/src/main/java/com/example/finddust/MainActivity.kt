a7a58c7a94697e5872b3a32962a143bfe0df0b97
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-09-25 13:44:33 +0900

    Add | Generate api repository

--- app/src/main/java/com/example/finddust/MainActivity.kt
+++ app/src/main/java/com/example/finddust/MainActivity.kt
@@ -6,17 +6,22 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import com.example.finddust.data.Repository
 import com.example.finddust.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private var cancellationTokenSource: CancellationTokenSource? = null
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val scope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,26 +34,23 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         cancellationTokenSource?.cancel()
+        scope.cancel()
     }
 
-    @SuppressLint("MissingPermission")
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        val locationPermissionGranted = requestCode == REQUEST_ACCESS_LOCATION_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED
 
         if (!locationPermissionGranted) {
             finish()
         } else {
-            cancellationTokenSource = CancellationTokenSource()
-            cancellationTokenSource?.let {
-                fusedLocationProviderClient.getCurrentLocation(
-                        LocationRequest.PRIORITY_HIGH_ACCURACY,
-                        it.token
-                ).addOnSuccessListener { location ->
-                    binding.textView.text = "${location.latitude} : ${location.longitude}"
-                }
-            }
+            fetchAirQualityData()
         }
     }
 
@@ -58,13 +60,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun requestLocationPermission() {
         ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                REQUEST_ACCESS_LOCATION_PERMISSION
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSION
         )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun fetchAirQualityData() {
+        cancellationTokenSource = CancellationTokenSource()
+        cancellationTokenSource?.let {
+            fusedLocationProviderClient
+                .getCurrentLocation(
+                    LocationRequest.PRIORITY_HIGH_ACCURACY,
+                    it.token
+                ).addOnSuccessListener { location ->
+                    scope.launch {
+                        val monitoringStation = Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
+                        binding.textView.text = monitoringStation?.stationName
+                    }
+                }
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/example/finddust/data/Repository.kt
@@ -0,0 +1,66 @@
+package com.example.finddust.data
+
+import com.example.finddust.BuildConfig
+import com.example.finddust.data.models.monitoringstation.MonitoringStation
+import com.example.finddust.data.services.AirKoreaApiService
+import com.example.finddust.data.services.KakaoLocalApiService
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object Repository {
+
+    suspend fun getNearbyMonitoringStation(latitude: Double, longitude: Double): MonitoringStation? {
+        val tmCoordinates = kakaoLocalApiService
+            .getTmCoordinates(longitude, latitude)
+            .body()
+            ?.documents
+            ?.firstOrNull()
+
+        val tmX = tmCoordinates?.x
+        val tmY = tmCoordinates?.y
+
+        return airKoreaApiService
+            .getNearbyMonitoringStation(tmX!!, tmY!!)
+            .body()
+            ?.response
+            ?.body
+            ?.monitoringStations
+            ?.minByOrNull { it.tm ?: Double.MAX_VALUE }
+    }
+
+    private val kakaoLocalApiService: KakaoLocalApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(Url.KAKAO_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(buildHttpClient())
+            .build()
+            .create()
+    }
+
+    private val airKoreaApiService: AirKoreaApiService by lazy {
+
+        Retrofit.Builder()
+            .baseUrl(Url.AIR_KOREA_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(buildHttpClient())
+            .build()
+            .create()
+    }
+
+    private fun buildHttpClient(): OkHttpClient =
+            OkHttpClient.Builder()
+                .addInterceptor(
+                    HttpLoggingInterceptor().apply {
+                        level = if (BuildConfig.DEBUG) {
+                            HttpLoggingInterceptor.Level.BODY
+                        } else {
+                            HttpLoggingInterceptor.Level.NONE
+                        }
+                    }
+                ).build()
+}
